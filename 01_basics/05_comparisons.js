5709b36cdb4a92642acd984f79495b4bb37a7f67
Shahnawaz Akhtar Ansari <73360721+shahnawazakhtaransari@users.noreply.github.com>
2024-05-07 21:48:37 +0000

    comparison

---
+++ 01_basics/05_comparisons.js
@@ -0,0 +1,16 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1)
+
+//console.log("1"<2);
+//console.log("02">1);
+
+//console.log(null == 0);returns false
+//console.log(null >= 0); //returns true....comparison operators converts null to a number 
+//console.log(null>0); returns false
+
+//=== does strict check
+console.log("2"===2); //false as they have diff datatype
+console.log("2"==2); //true

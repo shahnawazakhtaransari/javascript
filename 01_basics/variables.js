19d9cb9c643791adc526520aeb09a2359f068523
Shahnawaz Akhtar Ansari <73360721+shahnawazakhtaransari@users.noreply.github.com>
2024-05-07 15:03:00 +0000

    container config done

---
+++ 01_basics/variables.js
@@ -0,0 +1,15 @@
+const accountId=9787
+let accountEmail="iamshaz786@gmail.com"
+var accountPassword="12345"
+accountCity="kolkata"
+
+//accountId=2
+accountEmail="heheh@yahoo.com"
+accountPassword="67890"
+accountCity="Howrah"
+let accountState;
+console.log(accountId);
+console.table([accountEmail,accountPassword,accountCity,accountState])
+/*nowadays we dont use var but we use let..because there was problem 
+ in block scope and functional scope
+ therefore we use let for variable type of data*/

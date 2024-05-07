e603a03961724454f3f9f0a949b2ca522ba71e3c
Shahnawaz Akhtar Ansari <73360721+shahnawazakhtaransari@users.noreply.github.com>
2024-05-07 19:23:48 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,20 @@
+"use strict";//treat all js code as newer version
+//alert(3+3) we are using nodejs here not browser
+let namee="Nawaz"
+let age=23
+let isloggedIn=false
+console.log(typeof namee)
+console.log(typeof age)
+console.log(typeof isloggedIn)
+// ----Primitive Datatypes-----
+//number=> 2 to power of 53
+//bigint usse bade number k liye
+//string => " "
+//boolean =>true or false
+//null. its a standalone value in js
+console.log(typeof null) //prints object...sometimes asked in interviews
+//undefined =>
+//symbol => unique
+
+//-----non primitive------
+//object

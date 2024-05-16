3a5fb5167f1323d73c5da855bfc6b40124f187c4
Shahnawaz Akhtar Ansari <73360721+shahnawazakhtaransari@users.noreply.github.com>
2024-05-16 15:31:13 +0000

    objects

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -7,3 +7,45 @@ tinderUser.name="Aman"
 tinderUser.isLoggedin=false
 console.log(tinderUser);
 
+const regularUser={
+    email:"some@gmail.com",
+    fullname:{
+        userfullname:{
+            firstname:"Shahnawaz",
+            lastname:"Ansari"
+        }
+    }
+}
+// console.log(regularUser.fullname);
+// console.log(regularUser.fullname?.userfullname.firstname);// ? is used to check if we have got that value then print firstname otherwise dont!
+const obj1={1:"a",2:"b"}
+const obj2={3:"a",4:"b"}
+//const obj3={obj1,obj2}//object ke andar objects banjaenge..not recommended...............1st method of joining
+//const obj3= Object.assign({},obj1,obj2)//watch video again for using empty{} reason ...........2nd method of joining
+//console.log(obj3);   
+
+const obj3={...obj1,...obj2}//spread operator mostly used..and easiest of them all
+console.log(obj3);
+
+const users=[
+    {
+        id:1,
+        email:"s@gmail.com"
+    },
+    {
+        id:1,
+        email:"s@gmail.com"
+    },
+    {
+        id:1,
+        email:"s@gmail.com"
+    },
+]
+
+users[1].email
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));//prints as error..very much used in project
+console.log(Object.values(tinderUser));//printing values the same way as keys
+console.log(Object.entries(tinderUser));//used lesser
+console.log(tinderUser.hasOwnProperty('name'));//good practice to check for a property without actually using it

bc2decdd6a38971fef4c32bec8bc435086c324db
Shahnawaz Akhtar Ansari <73360721+shahnawazakhtaransari@users.noreply.github.com>
2024-05-17 22:02:53 +0000

    scopes

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,48 @@
+// {}// it is the scope
+// //var c =300
+
+// if(true){
+//     let a=10
+//     const b=20
+//     var c=30// a or b cant be printed as they are defined only in the scope of if...var is also declared in if but it shows the output = this is the reason we dont use var
+// }
+
+// //console.log(a);
+// //console.log(b);
+// console.log(c);
+
+//            2nd explaination
+// let a=300//global scope
+// if (true){
+//     a=30//block scope
+//     console.log("INNER: ",a);
+// }
+
+// console.log(a);
+
+//...next lecturee
+
+function one(){
+    const username="Shahnawaz"
+
+    function two(){
+        const website="Github"
+        console.log(username);
+
+    }
+    //console.log(website);
+    two()
+}
+one()
+//inner function can access outer function but outer function cant access inner function
+if(true){
+    const username="Ansari"
+    const website="Google"
+    console.log(username+" "+website);
+}
+//===========interesting=============
+function addOne(num){
+    return num+1
+}
+addOne(100)
+console.log(addOne());

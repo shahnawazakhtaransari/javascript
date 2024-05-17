89870732a839910df13826c10a10f2934950b274
Shahnawaz Akhtar Ansari <73360721+shahnawazakhtaransari@users.noreply.github.com>
2024-05-17 21:27:12 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -29,3 +29,33 @@ function loginUser(username="Aman"){ //if no username value is given by the user
     return`${username} just logged in`
 }
 console.log(loginUser("shahnawaz"));
+//**********************lec-20******************** */
+function calculateCartPrice(...num1){//rest operator..if ni lagaenge toh only first value print hoga
+    return num1
+}
+console.log(calculateCartPrice(200,400,500));
+
+function calculateCartPrice2(val1,val2,...num1){//val1 and val2 takes the first two arguments and the rest are past as array using rest operator(...)
+    return num1
+}
+console.log(calculateCartPrice2(200,400,500,5432,7543));
+
+const user={
+    usename:"aman",
+    price:499
+}
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.usename} and price is ${anyobject.price}`);
+}
+//handleObject(user)
+handleObject({
+    usename:"Sam",
+    price:199
+})
+
+const arr=[100,200,300,400]
+function SecondValue(getArray){
+    return getArray[1]
+}
+console.log(SecondValue(arr));
+console.log(SecondValue([100,200,300,400,500,600]));//works the same directly too

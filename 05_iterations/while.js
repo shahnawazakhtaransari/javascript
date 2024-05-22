923973ff73ba7f872468aab05c3b14a62156a255
Shahnawaz Akhtar Ansari <73360721+shahnawazakhtaransari@users.noreply.github.com>
2024-05-22 11:10:02 +0000

    while-do while

---
+++ 05_iterations/while.js
@@ -0,0 +1,19 @@
+//while   do-while
+let i=0
+while (i<=10) {
+    //console.log(`Value of i is ${i}`);
+    i++;
+}
+let myArray = ['flash', "batman", "superman"]
+
+let arr = 0
+while (arr < myArray.length) {
+    //console.log(`Value is ${myArray[arr]}`);
+    arr++
+}
+let score = 11
+
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
